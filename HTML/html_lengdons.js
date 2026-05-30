42fcc3bfd9d17793820bc059ea790cc784ef2714
Lengdons <kleinbergsrihards@gmail.com>
2026-05-30 21:55:47 +0300

    FP_demo2

---
+++ HTML/html_lengdons.js
@@ -0,0 +1,23 @@
+
+const copy = document.getElementById('datu-kaste');
+
+const numberOfTimes = 5; 
+
+let PieejamsRindas = '';
+
+for (let i = 0; i < 40; i++) {
+    
+    PieejamsRindas += `
+        <div class="datu-rinda">
+            <div class="data-box">Kabinets Nr: ${i + 1}</div>
+            <div class ="data-box"> Vietas kabinetā: </div>
+            <div class="data-box">Lietotājs</div>
+            <div class="data-box">Paņēma: </div>
+            <div class="status-ind green">
+                <span class="status-txt">Pieejams</span>
+            </div>
+            <div class="data-box">Nodeva: </div>
+        </div>
+    `;
+}
+copy.innerHTML = PieejamsRindas;
